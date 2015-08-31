e776dcbc298a9756d6b27f878d6871bdcf46fcf7
oryasis <info@oryasis.net>
2015-08-31 13:38:04 +0000

    associate listings with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,14 +3,15 @@ class Listing < ActiveRecord::Base
       has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "default.jpg"
   else
     
-      has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "default.jpg"
-      validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/,
+      has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "default.jpg",
                                         :storage => :dropbox,
                                         :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
                                         :path => ":style/:id_/:filename"
   end
   validates :name, :description, :price, presence: true
   validates :price, numericality: {greater_than: 0}
-  validates :image, attachment_presence: true
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  
+  belongs_to :user
 end
 
